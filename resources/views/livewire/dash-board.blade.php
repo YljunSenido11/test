038045bfd2676b2f3ec38a81ba8f6a06b1fdea79
YLjunSenido <yljunsenido177@gmail.com>
2024-10-21 10:21:08 +0800

    Creating Register Page done

--- resources/views/livewire/dash-board.blade.php
+++ resources/views/livewire/dash-board.blade.php
@@ -7,3 +7,4 @@
             Increment</button>
     </div>
 </div>
+    
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/register', function () {
+        return view('livewire.register');
+    })->name('register');
 });
